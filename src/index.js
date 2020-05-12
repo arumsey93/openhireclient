81ef0ec0721943e9e16be8ef6efb26f2e4f76cde
Alex rumsey <alexrumsey@Alexs-MacBook-Pro.local>
2020-05-12 17:19:11 -0500

    updated meta

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import { BrowserRouter as Router } from "react-router-dom"
 import OpenHire from './components/OpenHire'
 import './index.css'
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-
 ReactDOM.render(
   <Router>
       <OpenHire />
